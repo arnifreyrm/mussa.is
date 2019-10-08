e0f04bde9a204f72b15f58b40bcf6057e782f2d8
Valtýr Örn Kjartansson <valtyr@gmail.com>
2019-10-08 14:23:11 +0000

    Flexa shitt í gang bru

--- src/templates/verk.js
+++ src/templates/verk.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "gatsby";
 import {imageUrl} from "../helpers/url"
 
 export default ({ pageContext: { verk } }) => (
--- src/templates/yfirlit.js
+++ src/templates/yfirlit.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { Link } from "gatsby";
+
 import {imageUrlCSS} from "../helpers/url"
+
 import Layout from "../components/Layout";
 import styles from "./yfirlit.module.scss"
 
 export default ({ pageContext: { ollVerk } }) => (
   <Layout activePage="yfirlit">
-    {ollVerk.map(verk => (
-      <Link to={`/verk/${verk.id}`}>
-        <div
-          className={styles.thumbnail}
-          style={{ backgroundImage: imageUrlCSS(verk.file)}}
-        >hhhh</div>
-      </Link>
-    ))}
+    <div className={styles.root}>
+        
+        {ollVerk.map(verk => (
+          <Link to={`/verk/${verk.id}`}>
+            <div
+              className={styles.thumbnail}
+              style={{ backgroundImage: imageUrlCSS(verk.file)}}
+            >hhhh</div>
+          </Link>
+        ))}
+    </div>
   </Layout>
 );
