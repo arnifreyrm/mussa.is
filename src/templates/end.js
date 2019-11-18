970a1d0c2c6d5b0a4bcb970c7b6493b1ebe1f6d2
Árni Freyr <afm7@hi.is>
2019-11-18 10:22:04 +0000

    Added title to endpage

--- src/templates/end.js
+++ src/templates/end.js
@@ -3,13 +3,14 @@ import { Link } from "gatsby";
 import styles from "./end.module.scss";
 export default ({ pageContext: { verk, prevVerk, nextVerk } }) => (
   <div className={styles.root}>
-      <div className={styles.text}>
-        <p>
-          Fleiri myndir í vændum
-        </p>
-        <div className={styles.img_container}>
-          <img src="../images/fullsize/mussa.png" className={styles.img} />
-        </div>
+    <head>
+      <title>Mússa</title>
+    </head>
+    <div className={styles.text}>
+      <p>Fleiri myndir í vændum</p>
+      <div className={styles.img_container}>
+        <img src="../images/fullsize/mussa.png" className={styles.img} />
       </div>
+    </div>
   </div>
 );
