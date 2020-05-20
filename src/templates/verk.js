d263666be64c2b328ccf55611bba030585b2c222
Árni Freyr <afm7@hi.is>
2020-05-20 15:45:33 +0000

    Preload bugfix

--- src/templates/verk.js
+++ src/templates/verk.js
@@ -8,7 +8,7 @@ export default ({ pageContext: { verk, prevVerk, nextVerk } }) => (
   <div className={styles.root}>
     <head>
       <title>Mússa</title>
-      <link rel="preload" href={imageUrl(nextVerk.file)} as="image"></link>
+      <link rel="preload" href={verk.file==="fullsize/img102.jpg" ? imageUrl(verk.file) : imageUrl(nextVerk.file)} as="image"></link>
     </head>
     <Link
       to={verk.file === "fullsize/img102.jpg" ? `/end` : verk.file === 'fullsize/img74.jpg' ? `/text`: `/verk/${nextVerk.id}`}
