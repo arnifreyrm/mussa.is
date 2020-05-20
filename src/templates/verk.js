d150cab74eb42acb59aef00db2b7ad81c88e5cf6
Árni Freyr <afm7@hi.is>
2020-05-20 15:30:09 +0000

    Add images and preload images

--- src/templates/verk.js
+++ src/templates/verk.js
@@ -8,9 +8,10 @@ export default ({ pageContext: { verk, prevVerk, nextVerk } }) => (
   <div className={styles.root}>
     <head>
       <title>Mússa</title>
+      <link rel="preload" href={imageUrl(nextVerk.file)} as="image"></link>
     </head>
     <Link
-      to={verk.file === "fullsize/img93.jpg" ? `/end` : verk.file === 'fullsize/img65.jpg' ? `/text`: `/verk/${nextVerk.id}`}
+      to={verk.file === "fullsize/img102.jpg" ? `/end` : verk.file === 'fullsize/img74.jpg' ? `/text`: `/verk/${nextVerk.id}`}
       style={{ textDecoration: "none", color: "black" }}
     >
       <div>
